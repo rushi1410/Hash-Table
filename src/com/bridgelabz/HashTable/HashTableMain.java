b9956fcdb07fb51612dbdf3339279cd2a680c24e
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-31 09:05:37 +0530

    Added:Find Frequency Of Words in a Sentence

--- src/com/bridgelabz/HashTable/HashTableMain.java
+++ src/com/bridgelabz/HashTable/HashTableMain.java
@@ -3,5 +3,20 @@ package com.bridgelabz.HashTable;
 public class HashTableMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Hash Table Program");
+		String paragraph = "To be or not to be";
+
+
+		String[] splitArray = paragraph.split(" ");
+		MyHashTable<String,Integer> hashTable = new MyHashTable<>();
+		for (String word:splitArray) {
+			if(hashTable.containsKey(word)){
+				int count =  hashTable.get(word);
+				hashTable.replace(word,count+1);
+			}
+			else{
+				hashTable.put(word,1);
+			}
+		}
+		hashTable.print();
 	}
 }
